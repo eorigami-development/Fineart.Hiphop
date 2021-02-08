8c4b78290b2dd431c26c003059638064d417db79
vinod <vinod@myorigami.co>
2021-02-08 19:46:16 +0530

    new changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
 import React from "react";
+// import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 const About = () => (
     <div className="wrapper">
-        <h2>This is the about page</h2>
+        <h2 className="myCLassNAme">This is the about page</h2>
+        <Button>SEND</Button>
     </div>
 );
 
