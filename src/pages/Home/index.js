224f783f438ee007fbf40632d07bca3532e2e635
Nihar <nihar@myorigami.co>
2021-02-09 12:34:29 +0530

    after changes

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CarouselComponent from "./../../components/CarouselComponent";
+
+const Home = () => {
+    return (
+        <div>
+            <CarouselComponent />
+        </div>
+    )
+}
+
+export default Home
