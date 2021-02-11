c4568a4487b3113eea2d2cd0e134b55843ce45e1
Nihar <nihar@myorigami.co>
2021-02-11 17:05:26 +0530

    New Components

--- src/components/FilterComponent/index.js
+++ src/components/FilterComponent/index.js
@@ -4,11 +4,6 @@ import { ReactComponent as UpArrowIcon } from "./../../assets/upArrow.svg"
 import "./styles.css"
 
 const FilterComponent = () => {
-    const [open, setOpen] = useState(false)
-
-    // const handleToggle = () => {
-    //     setActive(!isActive);
-    // };
     return (
         <div>
             <p className="filterByText">Filter by</p>
@@ -16,33 +11,13 @@ const FilterComponent = () => {
                 <Dropdown as={ButtonGroup}>
                     <Dropdown.Toggle id="artistDropdown" className="artistDropdown">Artist/Maker<UpArrowIcon className="upArrow" /></Dropdown.Toggle>
                     <Dropdown.Menu className="artistMenu">
-                        {/* <Dropdown eventKey="1"> */}
                             <Form.Check type="checkbox" label="Beastie Boys" />
                             <Form.Check type="checkbox" label="Buddy Esquire" />
                             <Form.Check type="checkbox" label="Chi Modu" />
                             <Form.Check type="checkbox" label="Daniel Arsham" />
                             <Form.Check type="checkbox" label="Dave Muller" />
-                            
-                        {/* </Dropdown> */}
-                        {/* <Dropdown eventKey="2">Another action</Dropdown>
-                        <Dropdown eventKey="3" active>
-                            Active Item
-                        </Dropdown>
-                        <Dropdown.Divider />
-                        <Dropdown eventKey="4">Separated link</Dropdown > */}
                     </Dropdown.Menu>
                 </Dropdown>
-                {/* <button
-                    aria-expanded={open === true ? "true" : "false"}
-                    className={open === true ? "active" : ""}
-                    onClick={() => setOpen(!open)}>
-                    Toggle
-                 </button>
-                <ul id="test" className={open ? "show" : ""}>
-                    <li>
-                        <a href="/some-link">Some Link</a>
-                    </li>
-                 </ul> */}
             </Col>
         </div>
     )
